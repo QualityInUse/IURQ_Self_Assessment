18635cd848d89d2b1fa85032802314d4db71243d
Saveliy Chertkov <s.chertkov@innopolis.university>
2024-11-01 17:51:04 +0000

    Update 2 files
    - /MainTask/PDFModule/PDFTest/Template (1).pdf
    - /MainTask/PDFModule/PDFTest/PDF_test.py

--- MainTask/PDFModule/PDFTest/PDF_test.py
+++ MainTask/PDFModule/PDFTest/PDF_test.py
@@ -1,7 +1,8 @@
+import os
 from iu_rq_self_assessment_bot.MainTask.PDFModule.PDFFile import PDF
 
 
-PDF_FILE_PATH = r"C:\Users\Saveliy\Downloads\Template.pdf"
+PDF_FILE_PATH = os.path.join(os.path.dirname(__file__), 'Template (1).pdf')
 
 
 def test_pdf_initialization():
@@ -19,7 +20,8 @@ def test_pdf_questions():
     questions = {
         'Q1 Tsui and Karam review questions': '',
         'Q1.1 Which decisions are those taken by the software engineer about the best ways '
-        '(processes, techniques and technologies) to achieve the requirements?': 'Your answer Your answer',
+        '(processes, techniques and technologies) to achieve the requirements?':
+            'Your answer. (Ex.: apple, banana, etc) Your answer',
         'Q1.2 Define the depth versus the breadth issue in software complexity.': 'Your answer',
         'Q1.3 List two technical concerns in developing large systems.': 'Your answer',
         'Q1.4 What is meant by the term principles of software engineering?': 'Your answer',
@@ -27,7 +29,7 @@ def test_pdf_questions():
         'Q2.1 Pressman discusses the problems of legacy software. What does he mean that'
         ' such software needs to be “re-engineered.” Give some specifics of re-engineering.': 'Your answer',
         'Q2.2 Pressman defines Software Engineering, so, what is the difference between '
-        'Software Engineering and Computer Science?': 'Your answer',
+        'Software Engineering and Computer Science?': 'Your answer (Example: one, two, three)',
         'Q2.3 Simply list the separate processes that a development team should manage. '
         'Also explain why each is, or is not important.': 'Your answer'
     }
